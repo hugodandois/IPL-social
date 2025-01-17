82f19ac5f4f889933faf1bc5093e741d6885578d
hugo dandois <hugo.dandois@student.vinci.be>
2025-01-17 16:57:50 +0100

    add function passwordAtLeast8char should return false with one charactere

--- spec/passwordChecker.test.js
+++ spec/passwordChecker.test.js
@@ -6,6 +6,10 @@ describe("PasswordChecker class should implement a password checker function", f
         it("given a password 12345678", function() {
             expect(PasswordChecker.containsAtLeast8chars("12345678")).toBe(true);
         });
+
+        it("given a password with 1 chararacter", function() {
+            expect(PasswordChecker.containsAtLeast8chars("1")).toBe(false);
+        });
         
     });
 
--- src/passwordChecker.js
+++ src/passwordChecker.js
@@ -1,5 +1,8 @@
 export class PasswordChecker {
     static containsAtLeast8chars(password) {
+        if(password.length == 1){
+            return false;
+        }
         return true;
         
     }
