cb85bb2adf7421b4aad22134edf655a04c75bc54
hugo dandois <hugo.dandois@student.vinci.be>
2025-01-17 16:54:03 +0100

    add function passwordAtLeast8char should return true

---
+++ spec/passwordChecker.test.js
@@ -0,0 +1,14 @@
+import { PasswordChecker } from "../src/passwordChecker.js";
+
+describe("PasswordChecker class should implement a password checker function", function() {
+
+    describe("PasswordChecker function at least 8 char ", function() {
+        it("given a password 12345678", function() {
+            expect(PasswordChecker.containsAtLeast8chars("12345678")).toBe(true);
+        });
+        
+    });
+
+});
+
+
---
+++ src/passwordChecker.js
@@ -0,0 +1,6 @@
+export class PasswordChecker {
+    static containsAtLeast8chars(password) {
+        return true;
+        
+    }
+}
